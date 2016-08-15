aed935039b50a2bf7e6a76b40be862fb275b649c
Jaime Giraldo <sposmen@gmail.com>
2016-08-15 08:59:52 -0500

    Movie checker refactor

--- app.rb
+++ app.rb
@@ -2,12 +2,14 @@ require 'sinatra'
 require './lib/adivine_pues'
 
 class Application < Sinatra::Base
-# Configs
-  use Rack::Session::Cookie, :key => 'rack.session', :path => '/', :secret => 'some-random-string'
 
+  # Configs
+
+  use Rack::Session::Cookie, :key => 'rack.session', :path => '/', :secret => 'some-random-string'
   set :public_folder, Proc.new { File.join(root, "static") }
 
-# Routes
+
+  # Routes
 
   get '/' do
     session[:next_page] = 1
@@ -15,50 +17,15 @@ class Application < Sinatra::Base
     erb :index, layout: :layout
   end
 
-  post '/next/1' do
-    session[:next_page] = 2
+  post '/next/:id' do
+    id = params[:id].to_i
+    session[:next_page] = id + 1
     game = session[:game]
-    result = game.verify_answer(0, params[:answer])
+    result = game.verify_answer(id - 1, params[:answer])
     game.set_hint_used params[:hint] if result
     session[:game] = game
-    erb :index, layout: :layout
-  end
-
-  post '/next/2' do
-    session[:next_page] = 3
-    game = session[:game]
-    result = game.verify_answer(1, params[:answer])
-    game.set_hint_used params[:hint] if result
-    session[:game] = game
-
-    erb :index, layout: :layout
-  end
-
-  post '/next/3' do
-    session[:next_page] = 4
-    game = session[:game]
-    result = game.verify_answer(2, params[:answer])
-    game.set_hint_used params[:hint] if result
-    session[:game] = game
-
-    erb :index, layout: :layout
-  end
-
-  post '/next/4' do
-    session[:next_page] = 5
-    game = session[:game]
-    result = game.verify_answer(3, params[:answer])
-    game.set_hint_used params[:hint] if result
-    session[:game] = game
-
-    erb :index, layout: :layout
-  end
-
-  post '/next/5' do
-    game = session[:game]
-    result = game.verify_answer(4, params[:answer])
-    game.set_hint_used params[:hint] if result
     session[:result] = game.get_final_score
-    erb :answer, layout: :layout
+    erb id != 5 ? :index : :answer, layout: :layout
   end
+
 end
